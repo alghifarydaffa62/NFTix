8f45441159850ebee3b087f2f5ba78218a2e1046
M Daffa A <daffa.alghifary62@gmail.com>
2025-10-02 17:38:38 +0700

    Feat: deleting template environtment

--- test/Counter.ts
+++ test/Counter.ts
@@ -3,34 +3,12 @@ import { network } from "hardhat";
 
 const { ethers } = await network.connect();
 
-describe("Counter", function () {
-  it("Should emit the Increment event when calling the inc() function", async function () {
-    const counter = await ethers.deployContract("Counter");
-
-    await expect(counter.inc()).to.emit(counter, "Increment").withArgs(1n);
+describe("Event Test", function () {
+  it("", async function () {
+    
   });
 
-  it("The sum of the Increment events should match the current value", async function () {
-    const counter = await ethers.deployContract("Counter");
-    const deploymentBlockNumber = await ethers.provider.getBlockNumber();
+  it("", async function () {
 
-    // run a series of increments
-    for (let i = 1; i <= 10; i++) {
-      await counter.incBy(i);
-    }
-
-    const events = await counter.queryFilter(
-      counter.filters.Increment(),
-      deploymentBlockNumber,
-      "latest",
-    );
-
-    // check that the aggregated events match the current value
-    let total = 0n;
-    for (const event of events) {
-      total += event.args.by;
-    }
-
-    expect(await counter.x()).to.equal(total);
   });
 });

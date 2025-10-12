616393a341dab6dce4769ae9f3250bac2ee05eac
M Daffa A <daffa.alghifary62@gmail.com>
2025-10-12 10:11:20 +0700

    Hardhat deploying

--- ignition/modules/deploy.ts
+++ ignition/modules/deploy.ts
@@ -1,4 +1,6 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import "@nomicfoundation/hardhat-ignition-ethers";
+import "@nomicfoundation/hardhat-ignition"
 
 export default buildModule("NFTIXModule", (m) => {
   const ticketNFT = m.contract("TicketNFT");

de6daf5448f72b4de5ac86087add35ec05fbf969
raulmaulidhino-dev <raulahmadm314@gmail.com>
2025-10-21 10:38:09 +0700

    feat(favicon): add favicon images and rename title

--- dApp/src/Component/Navbar.jsx
+++ dApp/src/Component/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import logo from "../assets/nftix.svg"
+import logo from "../assets/logo.png"
 
 export default function Navbar() {
     return( 
@@ -7,7 +7,7 @@ export default function Navbar() {
             <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
                 <Link to="/" className="flex items-center gap-2">
                     <div className="w-10 h-10 bg-gradient-to-br from-blue-600 to-indigo-600 rounded-lg flex items-center justify-center">
-                        <img src={logo} alt=""/> 
+                        <img src={logo} alt="NFTix Logo" className="rounded-lg" /> 
                     </div>
                     <span className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent">
                         NFTix
